b356e35ee784495cea8c5498c8e28a6bf2a868c9
Lakshan Pulendran <98449126+Lachoo28@users.noreply.github.com>
2025-09-15 13:11:41 +0530

    Update ApproveDonors.js

--- client/src/components/admin/ApproveDonors.js
+++ client/src/components/admin/ApproveDonors.js
@@ -77,7 +77,7 @@ const ApproveDonors = () => {
         <h2 className="text-3xl font-bold text-gray-800">Approve Donors</h2>
         <button
           onClick={() => navigate('/admin/create-donor')}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200"
+          className="bg-primaryGreen text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200"
         >
           Create Donor
         </button>
